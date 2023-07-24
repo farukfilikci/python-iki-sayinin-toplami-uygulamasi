8e7b3bd26da3cf296293238fc09abc2bfd4347ef
farukfilikci <135740843+farukfilikci@users.noreply.github.com>
2023-07-24 22:18:16 +0300

    Update .py

--- .py
+++ .py
@@ -5,7 +5,7 @@ def toplama():
         toplam = sayi1 + sayi2
         print("Sonuç: {:.2f}".format(toplam))
     except ValueError:
-        print("Hatalı giriş yaptınız. Lütfen sayıları düzgün bir şekilde girin..")
+        print("Hatalı giriş yaptınız. Lütfen sayıları düzgün bir şekilde girin...")
 
 if __name__ == "__main__":
     toplama()
